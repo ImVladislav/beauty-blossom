4109ae52fddcebb41284b7617af5bb85325044a0
ImVladislav <nafanya102@gmail.com>
2023-10-04 23:02:07 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,28 +11,47 @@ function App() {
     <ThemeProvider theme={theme}>
       <Routes>
         <Route path="/" element={<SharedLayout />}>
-          <Route>
-            <Route path="/login" element={<p>login Page</p>} />
-            <Route path="/registration" element={<p>registration Page</p>} />
-          </Route>
-          <Route path="/" element={<MainPage />} />
-          <Route>
-            <Route path="/category" element={<p>category Page</p>} />
-            <Route path="/brands" element={<p>brands Page</p>} />
-            <Route path="/news" element={<p>news Page</p>} />
-            <Route path="/discount" element={<p>discount Page</p>} />
-            <Route
-              path="/wholesaleProgram"
-              element={<p>wholesaleProgram Page</p>}
-            />
-          </Route>
-
-          <Route path="*" element={<p>тут ні**я нема скільки не дивись</p>} />
+          <Route path="/login" element={<p>login Page</p>} />
+          <Route path="/registration" element={<p>registration Page</p>} />
+          <Route path="/category" element={<p>category Page</p>} />
+          <Route path="/brands" element={<p>brands Page</p>} />
+          <Route path="/news" element={<p>news Page</p>} />
+          <Route path="/discount" element={<p>discount Page</p>} />
+          <Route path="/wholesaleProgram" element={<p>wholesaleProgram Page</p>} />
+          <Route path="*" element={<p>тут нічого немає, скільки не дивись</p>} />
         </Route>
       </Routes>
+      {/* <GlobalStyle /> */}
     </ThemeProvider>
   );
 }
 
 export default App;
 // rafc
+// function App() {
+//   return (
+//     <ThemeProvider theme={theme}>
+//       <Routes>
+//         <Route path="/" element={<SharedLayout />}>
+//           <Route>
+//             <Route path="/login" element={<p>login Page</p>} />
+//             <Route path="/registration" element={<p>registration Page</p>} />
+//           </Route>
+//           <Route path="/" element={<MainPage />} />
+//           <Route>
+//             <Route path="/category" element={<p>category Page</p>} />
+//             <Route path="/brands" element={<p>brands Page</p>} />
+//             <Route path="/news" element={<p>news Page</p>} />
+//             <Route path="/discount" element={<p>discount Page</p>} />
+//             <Route
+//               path="/wholesaleProgram"
+//               element={<p>wholesaleProgram Page</p>}
+//             />
+//           </Route>
+
+//           <Route path="*" element={<p>тут ні**я нема скільки не дивись</p>} />
+//         </Route>
+//       </Routes>
+//     </ThemeProvider>
+//   );
+// }
