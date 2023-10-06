5a97559a1120bff6e2d6f4d314db49a18c5326ee
Iryna Bodnariuk <bodnariuk.iryna1@gmail.com>
2023-10-06 16:37:04 +0300

    fix: new fix export

--- src/Pages/MainPage/MainPage.jsx
+++ src/Pages/MainPage/MainPage.jsx
@@ -1,22 +1,30 @@
-
 import { Container } from "../../modules/components/Container";
-import Banner from "../../modules/components/Images/bannerPopularBrands.jpg"
+import Banner from "../../modules/components/Images/bannerPopularBrands.jpg";
 import { ProductSlider } from "../../modules/components/ProductSlider/ProductSlider";
-import { MainBanner, BannerTitle, StyledLink, Thumb, Background} from "./MainPageStyled";
+import {
+  MainBanner,
+  BannerTitle,
+  StyledLink,
+  Thumb,
+  Background,
+} from "./MainPageStyled";
 
-
-export const MainPage = () => {
-
-    return (<Background>
-        <Container>
-            <MainBanner src={Banner} alt="banner"/>
-            <Thumb>
-                <BannerTitle>Популярні <br /> корейські бренди</BannerTitle>
-                <StyledLink to="/terms_of_wholesale_cooperation">УМОВИ ОПТОВОЇ СПІВПРАЦІ</StyledLink>
-            </Thumb>
-        </Container>
-        <ProductSlider />
-
-        </Background>
-    );
-}
+const MainPage = () => {
+  return (
+    <Background>
+      <Container>
+        <MainBanner src={Banner} alt="banner" />
+        <Thumb>
+          <BannerTitle>
+            Популярні <br /> корейські бренди
+          </BannerTitle>
+          <StyledLink to="/terms_of_wholesale_cooperation">
+            УМОВИ ОПТОВОЇ СПІВПРАЦІ
+          </StyledLink>
+        </Thumb>
+      </Container>
+      <ProductSlider />
+    </Background>
+  );
+};
+export default MainPage;
