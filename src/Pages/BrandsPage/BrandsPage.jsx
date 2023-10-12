747b19ce0631584618b0694efd86236f7cfedfbc
ImVladislav <nafanya102@gmail.com>
2023-10-12 22:06:33 +0300

    1
    1

--- src/Pages/BrandsPage/BrandsPage.jsx
+++ src/Pages/BrandsPage/BrandsPage.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import Brands from "../../modules/components/Brands/Brands";
-import Category from "../../modules/components/Category/Category";
+
 import { useLocation } from "react-router-dom";
 import AllBrands from "../../modules/components/AllBrands/AllBrands";
 
+
+
+
+
+
 const BrandsPage = () => {
   const { pathname } = useLocation();
+
+ 
+
+ 
+
+
+
+
 
   return <>{pathname !== "/brands" ? <Brands /> : <AllBrands />}</>;
 };
