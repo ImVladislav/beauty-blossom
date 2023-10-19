4fb699b225c51b6d7d78c2a1b9321ac25ec8896c
Iryna Bodnariuk <bodnariuk.iryna1@gmail.com>
2023-10-19 20:25:36 +0300

    fix: changed the cart styles

--- src/modules/Header/ShopingList/ShopingListStyled.js
+++ src/modules/Header/ShopingList/ShopingListStyled.js
@@ -3,7 +3,6 @@ import styled from "styled-components";
 import { BsFillTrash3Fill } from "react-icons/bs";
 import { Link } from "react-router-dom";
 
-
 export const DeleteIcon = styled(BsFillTrash3Fill)`
   width: 15px;
   height: 15px;
@@ -14,19 +13,16 @@ export const DeleteIcon = styled(BsFillTrash3Fill)`
   }
 `;
 
-
 export const OpenModal = styled.button`
-
-    border: none;
-    background: transparent;
-    cursor: pointer;
+  border: none;
+  background: transparent;
+  cursor: pointer;
   & svg {
     &:hover,
     &:focus {
       color: ${(p) => p.theme.colors.accentColor};
     }
   }
-  
 `;
 
 export const ModalShopingList = styled.div`
@@ -35,16 +31,14 @@ export const ModalShopingList = styled.div`
   margin-left: auto;
   margin-right: auto;
   top: 100px;
-   background-color: white;
+  background-color: white;
 
-    padding: 30px;
+  padding: 30px;
 
-    box-shadow: 0 5px 35px rgba(0,0,0,0.75);
+  box-shadow: 0 5px 35px rgba(0, 0, 0, 0.75);
+`;
 
-`
-
-
-export const ModalBackground= styled.div`
+export const ModalBackground = styled.div`
   position: fixed;
   top: 0;
   left: 0;
@@ -55,82 +49,72 @@ export const ModalBackground= styled.div`
   justify-content: center; /* Центруємо горизонтально */
   align-items: center; /* Центруємо вертикально */
   z-index: 3;
-  `
+`;
 
-  
-export const ModalTitle= styled.h2`
+export const ModalTitle = styled.h2`
   font-size: 32px;
-    font-weight: normal;
-    margin: -6px 0 12px -2px;
-  `
+  font-weight: normal;
+  margin: -6px 0 12px -2px;
+`;
 
-  export const HeaderContent= styled.th`
+export const HeaderContent = styled.th`
   padding-bottom: 8px;
-    color: #898989;
-    font-size: 11px;
-  `
+  color: #898989;
+  font-size: 11px;
+`;
 
-
-   export const GoodsBlock= styled.tbody`
+export const GoodsBlock = styled.tbody`
   padding-bottom: 8px;
-    color: #898989;
-    font-size: 11px;
+  color: #898989;
+  font-size: 11px;
+`;
 
-    
-  `
-
-
-  export const ImageBlock= styled.td`
-
-    color: #898989;
-    font-size: 11px;
-    /* border: 1px solid red; */
-    border-bottom: 1px solid #f5f2ea;;
-    border-top: 1px  solid #f5f2ea;;
-    border-left:  1px  solid #f5f2ea;;
-    width: 106px;
-  `
-    export const DescriptionBlock= styled.td`
-
-    color: #898989;
-    font-size: 11px;
-        border-bottom: 1px solid #f5f2ea;
-    border-top: 1px  solid #f5f2ea;
-    width: 392px;
-    height: 113px;
-        /* &::after{
+export const ImageBlock = styled.td`
+  color: #898989;
+  font-size: 11px;
+  /* border: 1px solid red; */
+  border-bottom: 1px solid #f5f2ea;
+  border-top: 1px solid #f5f2ea;
+  border-left: 1px solid #f5f2ea;
+  width: 106px;
+`;
+export const DescriptionBlock = styled.td`
+  color: #898989;
+  font-size: 11px;
+  border-bottom: 1px solid #f5f2ea;
+  border-top: 1px solid #f5f2ea;
+  width: 392px;
+  height: 113px;
+  /* &::after{
         content: '';
     height: 5px;
     display: block;
     } */
-  `
-    export const AmountBlock= styled.td`
+`;
+export const AmountBlock = styled.td`
+  color: #898989;
 
-    color: #898989;
-    font-size: 11px;
-        border-bottom: 1px solid #f5f2ea;
-    border-top: 1px  solid #f5f2ea;
-    width: 100px;
+  border-bottom: 1px solid #f5f2ea;
+  border-top: 1px solid #f5f2ea;
+  width: 100px;
+`;
+export const PriceBlock = styled.td`
+  color: #898989;
+  font-size: 18px;
+  border-bottom: 1px solid #f5f2ea;
+  border-top: 1px solid #f5f2ea;
+  border-right: 1px solid #f5f2ea;
+  width: 130px;
+  text-align: center;
+`;
 
-  `
-    export const PriceBlock= styled.td`
-
-    color: #898989;
-    font-size: 11px;
-      border-bottom: 1px solid #f5f2ea;
-    border-top: 1px  solid #f5f2ea;
-    border-right:  1px  solid #f5f2ea;
-    width: 130px;
-    text-align: center;
-  `
-
-  export const DeleteBtn= styled.button`
+export const DeleteBtn = styled.button`
   position: absolute;
   right: 730px;
-    top: 45px;
+  top: 45px;
   border: none;
   background: transparent;
-  `
+`;
 
 export const Thumb = styled.tr`
   position: relative;
@@ -141,11 +125,10 @@ export const Thumb = styled.tr`
     /* Наприклад, змінюємо фон або кольори тексту */
     background-color: #f0f0f0;
     color: #333;
-    
+
     /* Застосовуємо стилі до внутрішнього DeleteBtn */
     ${DeleteIcon} {
       fill: red; /* Змінюємо колір DeleteBtn на червоний */
-      
     }
   }
 
@@ -163,81 +146,85 @@ export const Thumb = styled.tr`
     background-repeat: no-repeat;
     background-size: cover;
     transition: color 0.3s ease-in-out;
-    
   }
 `;
-
-
 
 export const ItemNameLink = styled(Link)`
   color: black;
   text-decoration: none;
+  font-size: 18px;
 
-    &:hover {
+  &:hover {
     color: ${(p) => p.theme.colors.accentColor};
   }
 `;
 
-  export const DecIncBtn= styled.button`
+export const DecIncBtn = styled.button`
   flex: 1 0 auto;
-    height: 100%;
-    position: relative;
-    display: inline-block;
-    vertical-align: top;
-    cursor: pointer;
-    color: #000;
-    border: 0;
-    padding: 0;
-    background: none;
-    font-size: 20px;
-      &:hover {
+  height: 100%;
+  position: relative;
+  display: inline-block;
+  vertical-align: top;
+
+  cursor: pointer;
+  color: #000;
+  border: 0;
+  padding: 0;
+  background: none;
+  font-size: 20px;
+  &:hover {
     color: ${(p) => p.theme.colors.accentColor};
     cursor: pointer;
   }
-  `
-  
-  export const CounterBlock= styled.div`
- border: 1px solid #f5f2ea;
-     width: 92px;
-    height: 38px;
-        display: flex;
-    border-radius: 4px;
-    border: 1px solid #acacac;
-    position: relative;
-    align-items: center;
-      &:hover {
+`;
+
+export const CounterBlock = styled.div`
+  border: 1px solid #f5f2ea;
+  width: 92px;
+  height: 38px;
+  display: flex;
+  border-radius: 4px;
+  border: 1px solid #acacac;
+  position: relative;
+  align-items: center;
+  font-size: 18px;
+  &:hover {
     color: ${(p) => p.theme.colors.accentColor};
     cursor: pointer;
-    
   }
-  `
+`;
 
-    
-  export const ButtonContainer= styled.div`
-display: inline;
+export const ButtonContainer = styled.div`
+  display: inline;
+`;
 
-  `
+export const Amount = styled.p`
+  text-align: end;
+  font-size: 24px;
+`;
+export const TextNoProduckts = styled.p`
+  margin: 30px;
+  color: ${(p) => p.theme.colors.black};
 
-    export const Amount= styled.p`
-text-align: end;
-
-  `
-  export const ReturnBTN= styled.button`
+  font-size: 30px;
+  text-align: center;
+`;
+export const ReturnBTN = styled.button`
   flex: 1 0 auto;
-    height: 100%;
-    position: relative;
-    display: inline-block;
-    vertical-align: top;
-    cursor: pointer;
-    color: #000;
-    border: none;
-    padding: 0;
-    background: none;
-    font-size: 18px;
-    width: 258px;
-    height: 36px;
-      &:hover {
+  height: 100%;
+  position: relative;
+  display: inline-block;
+  vertical-align: top;
+  cursor: pointer;
+  color: #000;
+  border: none;
+  padding: 0;
+  background: none;
+  font-size: 18px;
+  width: 258px;
+  height: 36px;
+  &:hover {
     color: ${(p) => p.theme.colors.accentColor};
     cursor: pointer;
   }
-  `
+`;
