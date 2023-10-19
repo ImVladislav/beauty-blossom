5bba3ec779a7428b13c705b9bbf0c05d3ba6d1ed
ImVladislav <nafanya102@gmail.com>
2023-10-19 20:03:48 +0300

    Update LogIn.jsx

--- src/modules/Header/LogIn/LogIn.jsx
+++ src/modules/Header/LogIn/LogIn.jsx
@@ -3,6 +3,8 @@ import { Link, LogInIcon, DropDown, ListItem, Modal, ModalBackdrop, LoginPasswor
 import { CloseButton } from "../../../shared/components/ReusebleCompoments/ModalCloseBTN/CloseButton";
 import LoginForm from "./LoginForm";
 import RegisterForm from "./RegisterForm";
+import { loggedInSelector, userSelector } from "../../../redux/auth/selectors";
+import { useSelector } from "react-redux";
 
 const LogIn = () => {
   const [showDropdown, setShowDropdown] = useState(false);
@@ -16,12 +18,59 @@ const LogIn = () => {
   const toggleModal = () => {
     setShowModal(true);
   };
+
+  const items = useSelector(userSelector);
+  console.log(items);
+  const isLogin = useSelector(loggedInSelector);
+  console.log(isLogin);
+
+  const handleLinkHover = () => {
+    if (isLogin) {
+      toggleDropdown();
+    }
+  };
+
+  const handleDropdownEnter = () => {
+    if (!isLogin) {
+      setShowModal(true);
+    }
+  };
+
+  const handleDropdownLeave = () => {
+    if (!isLogin) {
+      toggleDropdown();
+    }
+  };
+
   
+
   return (
     <>
-      <Link onClick={toggleModal}>
-        <LogInIcon />
-      </Link>
+<Link
+  onClick={isLogin ? toggleModal : null}
+  onMouseEnter={isLogin ? null : handleLinkHover}
+  onMouseLeave={isLogin ? null : handleDropdownLeave}
+>
+  <LogInIcon />
+</Link>
+          <DropDown >
+          <ul>
+            <ListItem>
+              <a href="/beauty-blossom/cabinet/">Особисті дані</a>
+            </ListItem>
+            <ListItem>
+              <a href="/beauty-blossom/cabinet/history">Замовлення</a>
+            </ListItem>
+            <ListItem>
+              <a href="/beauty-blossom/cabinet/favorite">Список бажань</a>
+            </ListItem>
+            <ListItem>
+              <a href="/">Вихід</a>
+            </ListItem>
+          </ul>
+        </DropDown>
+            {/* <p>{items}</p> */}
+
 
 {showModal && (
   <ModalBackdrop>
@@ -32,7 +81,7 @@ const LogIn = () => {
             </Link>/<Link onClick={() => setLoginOrRegister(false)}>
               
                 <LoginPasswordToggle>Реєстрація</LoginPasswordToggle>
-              </Link>
+            </Link>
       {loginOrRegister ? (
 
           <LoginForm/>
