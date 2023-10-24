be5bb58958e7661a9586dc16e4b5e707d6f93ecd
Iryna Bodnariuk <bodnariuk.iryna1@gmail.com>
2023-10-24 20:48:29 +0300

    feat: added products filtering

--- src/shared/components/ProductsList/ProductsList.styled.js
+++ src/shared/components/ProductsList/ProductsList.styled.js
@@ -6,3 +6,24 @@ export const ProductListContainer = styled.ul`
   /* margin: -6px; */
   /* max-width: 1440px; */
 `;
+
+export const FilterContainer = styled.div`
+  display: flex;
+  position: relative;
+  max-width: 1440px;
+  margin-left: auto;
+  margin-right: auto;
+  justify-content: flex-end;
+`;
+export const FilterWrap = styled.div`
+  display: flex;
+  justify-content: space_between;
+  align-items: center;
+  padding: 10px;
+`;
+
+export const FilterSelect = styled.select`
+  padding: 5px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+`;
