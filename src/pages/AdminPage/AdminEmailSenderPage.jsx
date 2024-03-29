e80553d1b44cd64183ebf8ea9b2c432c5271b340
ImVladislav <nafanya102@gmail.com>
2024-03-29 16:36:47 +0200

    ашч

--- src/pages/AdminPage/AdminEmailSenderPage.jsx
+++ src/pages/AdminPage/AdminEmailSenderPage.jsx
@@ -1,5 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const AdminEmailSenderPage = () => {
-  return <div>AdminEmailSenderPage</div>;
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const handleUpload = () => {
+    if (!selectedFile) {
+      alert("Please select a file.");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("image", selectedFile);
+
+    fetch("/upload", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Image uploaded:", data.imageUrl);
+        // Додаткові дії після успішного завантаження фото
+      })
+      .catch((error) => {
+        console.error("Error uploading image:", error);
+      });
+  };
+
+  return (
+    <div>
+      <h1>Admin Email Sender Page</h1>
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={handleUpload}>Upload Photo</button>
+    </div>
+  );
 };
