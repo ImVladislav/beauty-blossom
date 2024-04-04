69b6d9ff332d2941139f5e459e30ea879b57a7d4
ImVladislav <nafanya102@gmail.com>
2024-04-04 18:49:32 +0300

    add send email page

--- src/pages/AdminPage/AdminEmailSenderPage.jsx
+++ src/pages/AdminPage/AdminEmailSenderPage.jsx
@@ -1,16 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { toast } from "react-toastify";
+
+import {
+  Button,
+  Container,
+  DeleteButton,
+  Form,
+  Input,
+  Li,
+  Textarea,
+  Ul,
+} from "./AdminEmailSenderPageStyled";
 
 export const AdminEmailSenderPage = () => {
   const [files, setFiles] = useState([]);
+  const [title, setTitle] = useState("");
   const [text, setText] = useState("");
+  const [subject, setSubject] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [filter, setFilter] = useState("");
+  const [copiedContacts, setCopiedContacts] = useState([]);
+  const [toEmails, setToEmails] = useState([]);
 
   const handleFileChange = (e) => {
-    setFiles([...files, ...e.target.files]); // Додавання нових файлів до існуючого списку файлів
+    setFiles([...files, ...e.target.files]);
   };
 
   const handleTextChange = (e) => {
     setText(e.target.value);
+  };
+
+  const handleSubjectChange = (e) => {
+    setSubject(e.target.value);
+  };
+
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value);
   };
 
   const handleSubmit = async (e) => {
@@ -18,13 +44,18 @@ export const AdminEmailSenderPage = () => {
 
     const formData = new FormData();
     files.forEach((file) => {
-      formData.append("file", file); // Додавання кожного файлу до FormData
+      formData.append("file", file);
     });
+    formData.append("title", title);
     formData.append("text", text);
+    formData.append("to", JSON.stringify(toEmails));
+    formData.append("subject", subject);
 
     try {
       const response = await axios.post(
-        "http://localhost:3000/api/email/sendemail",
+        // "http://localhost:3000/api/email/sendemail",
+
+        "https://beauty-blossom-backend.onrender.com/api/email/sendemail",
         formData,
         {
           headers: {
@@ -32,24 +63,140 @@ export const AdminEmailSenderPage = () => {
           },
         }
       );
-      console.log(response.data);
+      toast.info("Пошта успішно відправлена");
+      setCopiedContacts([]);
     } catch (error) {
       console.error("Помилка:", error);
     }
   };
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.post(
+          // "http://localhost:3000/api/searchUser",
+
+          "https://beauty-blossom-backend.onrender.com/api/searchUser",
+
+          {
+            filter,
+          }
+        );
+        setSearchResults(response.data);
+      } catch (error) {
+        console.error("Помилка при отриманні результатів:", error);
+      }
+    };
+
+    if (filter.trim() !== "") {
+      fetchData();
+    } else {
+      setSearchResults([]);
+    }
+  }, [filter]);
+
+  const handleCopyContact = (firstName, lastName, email, number) => {
+    const copiedContact = `${firstName} ${lastName}, ${email}, ${number}`;
+    if (!copiedContacts.find((contact) => contact === copiedContact)) {
+      setCopiedContacts([...copiedContacts, copiedContact]);
+      setToEmails([...toEmails, email]);
+    }
+  };
+
+  const handleDeleteContact = (index) => {
+    const updatedContacts = [...copiedContacts];
+    updatedContacts.splice(index, 1);
+    setCopiedContacts(updatedContacts);
+
+    const updatedToEmails = [...toEmails];
+    updatedToEmails.splice(index, 1);
+    setToEmails(updatedToEmails);
+  };
+
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="file" onChange={handleFileChange} multiple />
-      {/* Дозволяє вибрати декілька файлів */}
-      <input
+    <Container>
+      <Form onSubmit={handleSubmit}>
+        <Input
+          type="text"
+          name="subject"
+          value={subject}
+          onChange={handleSubjectChange}
+          placeholder="Тема листа"
+        />
+        <Input
+          type="text"
+          name="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Привітання"
+        />
+        <Textarea
+          name="text"
+          value={text}
+          onChange={handleTextChange}
+          placeholder="Введіть текст для листа"
+        />
+        <Input type="file" onChange={handleFileChange} multiple />
+        <Button type="submit" disabled={copiedContacts.length === 0}>
+          Відправити
+        </Button>
+      </Form>
+
+      <Input
         type="text"
-        value={text}
-        onChange={handleTextChange}
-        placeholder="Введіть текст для листа"
+        value={filter}
+        onChange={handleFilterChange}
+        placeholder="Фільтр за ім'ям, прізвищем, email, номером"
       />
-      <button type="submit">Відправити</button>
-    </form>
+
+      {filter.trim() !== "" && (
+        <Ul>
+          {searchResults
+            .filter(
+              (result) =>
+                result.firstName.toLowerCase().includes(filter.toLowerCase()) ||
+                result.lastName.toLowerCase().includes(filter.toLowerCase()) ||
+                result.email.toLowerCase().includes(filter.toLowerCase()) ||
+                result.number.toString().includes(filter)
+            )
+            .slice(0, 10)
+            .map((result) => (
+              <Li key={result._id}>
+                {result.firstName} {result.lastName}, {result.email},{" "}
+                {result.number}
+                <Button
+                  onClick={() =>
+                    handleCopyContact(
+                      result.firstName,
+                      result.lastName,
+                      result.email,
+                      result.number
+                    )
+                  }
+                >
+                  Додати
+                </Button>
+              </Li>
+            ))}
+        </Ul>
+      )}
+
+      {copiedContacts.length > 0 && (
+        <div>
+          <h3>Список контактів до відправки:</h3>
+          <Ul>
+            {copiedContacts.map((contact, index) => (
+              <Li key={index}>
+                {contact}
+                <DeleteButton onClick={() => handleDeleteContact(index)}>
+                  Видалити
+                </DeleteButton>
+              </Li>
+            ))}
+          </Ul>
+        </div>
+      )}
+    </Container>
   );
 };
 
---
+++ src/pages/AdminPage/AdminEmailSenderPageStyled.js
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Textarea = styled.textarea`
+  width: 100%;
+  min-height: 200px;
+  border-radius: 15px;
+  padding: 6px 10px;
+  margin-top: 10px;
+`;
+
+export const Input = styled.input`
+  border-radius: 15px;
+  padding: 3px 10px;
+  margin-top: 10px;
+`;
+
+export const Button = styled.button`
+  border-radius: 15px;
+  padding: 3px 10px;
+  margin-top: 10px;
+`;
+
+export const Li = styled.li`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 4px;
+  border-bottom: 1px solid #9797a5;
+  padding: 2px;
+`;
+
+export const Ul = styled.ul`
+  margin-top: 10px;
+  border: 1px solid #9797a5;
+  padding: 5px;
+  border-radius: 20px;
+`;
+
+export const DeleteButton = styled.button`
+  border-radius: 15px;
+  padding: 3px 10px;
+`;
